0ae86a6b9eedb4bdb215e6858f5675e3441e38dc
Xamweis <maxboe1993@gmail.com>
2022-11-29 18:06:11 +0100

    final with aliases

--- lab1.sql
+++ lab1.sql
@@ -6,6 +6,6 @@ SELECT title FROM film;
 SELECT DISTINCT name AS language FROM language 
 	JOIN film ON film.language_id = language.language_id;
 
-SELECT COUNT(*) FROM store;
-SELECT COUNT(*) FROM staff;
+SELECT COUNT(*) AS amount_of_stores FROM store;
+SELECT COUNT(*) AS amount_of_employees FROM staff;
 SELECT first_name FROM staff;
