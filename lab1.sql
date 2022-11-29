9acff15fbbbfa68a0f325a268069b886c4409388
Xamweis <maxboe1993@gmail.com>
2022-11-29 17:08:02 +0100

    changed selecting lang command

--- lab1.sql
+++ lab1.sql
@@ -1,8 +1,8 @@
 USE sakila;
 SELECT * FROM film, actor, customer;
 SELECT title FROM film;
-SELECT DISTINCT name AS language FROM film 
-	JOIN language ON film.language_id = language.language_id;
+SELECT DISTINCT name AS language FROM language 
+	JOIN film ON film.language_id = language.language_id;
 
 SELECT COUNT(*) FROM store;
 SELECT COUNT(*) FROM staff;
