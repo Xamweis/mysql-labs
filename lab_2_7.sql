385d384a4b63793fdbaa3fd0aaa5d93125b012a8
Xamweis <maxboe1993@gmail.com>
2022-12-01 11:12:52 +0100

    aliasing

--- lab_2_7.sql
+++ lab_2_7.sql
@@ -1,8 +1,12 @@
 USE sakila;
 
+#####################################################
+-- ALIASING is possible WITH and WITHOUT keword AS
+#####################################################
+
 -- 1) nums of films in categories
 SELECT category.name AS category, COUNT(*) AS count
-FROM category JOIN film_category ON film_category.category_id = category.category_id
+FROM category LEFT JOIN film_category ON film_category.category_id = category.category_id
 JOIN film ON film.film_id = film_category.film_id
 GROUP BY category.name;
 -- same as
@@ -15,7 +19,8 @@ GROUP BY c.name;
 -- 2) total of each staff in aug 05
 SELECT DISTINCT staff.first_name, staff.last_name, COUNT(*) OVER (PARTITION BY staff.staff_id) AS count
 FROM staff JOIN rental ON rental.staff_id = staff.staff_id
-WHERE CONVERT(rental_date, DATE) > CONVERT('2005-07-31', DATE) AND CONVERT(rental_date, DATE) < CONVERT('2005-09-01', DATE);
+WHERE CONVERT(rental_date, DATE) > CONVERT('2005-07-31', DATE) 
+AND CONVERT(rental_date, DATE) < CONVERT('2005-09-01', DATE);
 -- same as
 SELECT DISTINCT s.first_name, s.last_name, COUNT(*) OVER (PARTITION BY s.staff_id) AS count
 FROM staff s, rental r
@@ -46,12 +51,16 @@ ORDER BY count DESC
 LIMIT 3;
 
 -- 5) staff first last and adress
-SELECT staff.first_name, staff.last_name, address.address
-FROM staff JOIN address ON address.address_id = staff.address_id;
+SELECT s.first_name, s.last_name, a.address
+FROM staff s JOIN address a ON a.address_id = s.address_id;
 
 -- 6) each film and number of actors in it
 SELECT DISTINCT film.title, COUNT(*) OVER (PARTITION BY film.title) AS count
 FROM film JOIN film_actor ON film_actor.film_id = film.film_id;
+-- same as
+SELECT DISTINCT f.title, COUNT(*) OVER (PARTITION BY f.title) AS count
+FROM film f, film_actor fa
+WHERE fa.film_id = f.film_id;
 
 -- 7) total paid by each customer ordered by last name.
 SELECT DISTINCT customer.first_name, customer.last_name, 
