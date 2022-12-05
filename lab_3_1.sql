b79c21f29cd851a7bd9dcc8ecba265a3931ac72d
Xamweis <maxboe1993@gmail.com>
2022-12-05 22:21:59 +0100

    final questions :)

--- lab_3_1.sql
+++ lab_3_1.sql
@@ -10,8 +10,58 @@ SELECT * FROM staff;
 INSERT INTO staff(first_name, last_name, address_id, email, store_id, active, username, last_update)
 SELECT first_name, last_name, address_id, email, store_id, active, first_name, last_update
 FROM customer
-WHERE first_name = 'TAMMY' AND last_name = 'SANDERS';
+WHERE first_name = 'Tammy' AND last_name = 'Sanders';
 
--- 3) Add rental for "Academy Dinosaur" by Charlotte Hunter from Mike Hillyer at Store 1
+SELECT * FROM staff;
+
+-- 3) Add rental for "Academy Dinosaur" by Charlotte Hunter from Mike Hillyer (at Store 1)
+SELECT * FROM rental;
+-- store not in rental
+INSERT INTO rental(	inventory_id, 
+					customer_id, 
+					staff_id)
+VALUES (
+	(SELECT inventory_id
+		FROM film
+		RIGHT JOIN inventory USING (film_id)
+		LEFT JOIN store s USING (store_id)
+		LEFT JOIN rental r USING (inventory_id)
+		WHERE title LIKE 'ACADEMY DINOSAUR'
+		AND store_id = 2
+		GROUP BY inventory_id
+		HAVING (COUNT(r.rental_date)-COUNT(r.return_date)) = 0
+        LIMIT 1), -- query from lab_2_8_6 checking for availability, error if not available
+	(SELECT customer_id 
+		FROM customer
+		WHERE first_name = 'CHARLOTTE' AND last_name = 'HUNTER'), 
+	(SELECT staff_id
+		FROM staff 
+		WHERE first_name = 'Mike' AND last_name = 'Hillyer'));
+
+-- controlling:
 SELECT * FROM rental
--- not finished
+WHERE customer_id =
+(SELECT customer_id 
+		FROM customer
+		WHERE first_name = 'CHARLOTTE' AND last_name = 'HUNTER')
+ORDER BY rental_date DESC;
+
+-- copy has been returned
+DROP TABLE IF EXISTS unreturned_copy;
+CREATE TEMPORARY TABLE unreturned_copy AS
+(SELECT rental_id 
+FROM rental
+WHERE return_date IS NULL
+AND customer_id = (SELECT customer_id 
+					FROM customer
+					WHERE first_name = 'CHARLOTTE' AND last_name = 'HUNTER')
+ORDER BY rental_date DESC
+LIMIT 1);
+UPDATE rental
+SET return_date = CURRENT_TIMESTAMP()
+WHERE rental_id = (SELECT rental_id FROM unreturned_copy);
+
+-- delete rental
+DELETE FROM rental
+WHERE customer_id = 130
+AND inventory_id IN (5,6,7,8);
